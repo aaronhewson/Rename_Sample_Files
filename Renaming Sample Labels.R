bb3e0b665e76602a864b9a3cf50e09b0700f46a9
Aaron Hewson <aaronjhewson@gmail.com>
2025-06-04 16:18:59 +1200

    Simplifying code

--- Renaming Sample Labels.R
+++ Renaming Sample Labels.R
@@ -1,6 +1,4 @@
-##This script takes the file names provided by Gentayne to my new ID, as outlined in the relevant File Renaming excel sheet. 
-##The reason for this is that the Gentayne labels are not sufficient to distinguish JD samples - the row and position of the tree is needed as well.
-
+##This script renames filenames, as per the relevant File Naming excel sheet. This is needed prior to genotyping, and then also prior to B Allele Frequency plotting.
 
 #If packages not installed, run code below:
 install.packages("readxl")
@@ -11,18 +9,19 @@ library(readxl)
 library(fs)
 
 
-## Renaming JD Orchard Samples
-# Path to the Excel with renaming columns, and folder with CEL files to be renamed
+## Renaming sample filenames for genotyping. This ensures filenames are specific enough to distinguish samples.
+
+# Path to the Excel with renaming columns, and folder with .CEL genotype files to be renamed
 excel_path <- "File Naming.xlsx"
-file_dir <- "C:/Users/curly/Desktop/Apple Genotyping/Genotyping Data/JD Samples"
+file_dir <- "C:/Users/curly/Desktop/Apple Genotyping/Inputs/Genotype Files/Aggregate Samples"
 
 # Read Excel file
 rename_df <- read_excel(excel_path)
 
 # Loop through each row and rename the files
 for(i in seq_len(nrow(rename_df))){ 
-  old_name <- rename_df$Old[i] 
-  new_name <- rename_df$New[i] 
+  old_name <- rename_df$Gen_Old[i] 
+  new_name <- rename_df$Gen_New[i] 
   
   old_path <- file.path(file_dir, old_name)
   new_path <- file.path(file_dir, new_name)
@@ -37,9 +36,9 @@ for(i in seq_len(nrow(rename_df))){
 
 
 ##Renaming CNV Files
-# Path to the Excel with renaming columns, and folder with CEL files to be renamed
+# Path to the Excel with renaming columns, and folder with .txt CNV files to be renamed
 excel_path <- "File Naming.xlsx"
-file_dir <- "C:/Users/curly/Desktop/Apple Genotyping/Genotyping Data/JD BAF Plots/CNV Files"
+file_dir <- "C:/Users/curly/Desktop/Apple Genotyping/Results/B Allele Frequencies/Axiom CNV Output Files"
 
 # Read Excel file
 rename_df <- read_excel(excel_path)
@@ -60,26 +59,4 @@ for(i in seq_len(nrow(rename_df))){
   }
 }
 
-##Renaming PFR Genotyped Samples
-# Path to the Excel with renaming columns, and folder with CEL files to be renamed
-excel_path <- "File Naming.xlsx"
-file_dir <- "C:/Users/curly/Desktop/Apple Genotyping/Genotype Files/PFR Samples"
 
-# Read Excel file
-rename_df <- read_excel(excel_path)
-
-# Loop through each row and rename the files
-for(i in seq_len(nrow(rename_df))){ 
-  old_name <- rename_df$GenOld[i] 
-  new_name <- rename_df$GenNew[i] 
-  
-  old_path <- file.path(file_dir, old_name)
-  new_path <- file.path(file_dir, new_name)
-  
-  if (file_exists(old_path)) {
-    file_move(old_path, new_path)
-    message(paste("Renamed:", old_name, "->", new_name))
-  } else {
-    warning(paste("File not found:", old_name))
-  }
-}
